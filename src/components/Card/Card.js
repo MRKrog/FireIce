b4aa63132c5db29e04c5b6223dca3d5058ca4eb7
Michael Krog <michaelryankrog@gmail.com>
2019-04-16 11:05:01 -0600

    clean up of card ready for sworn member

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,32 +4,28 @@ const Card = ({ name, founded, seats, titles, coatOfArms, ancestralWeapons, word
   return (
     <div className="Card">
       <div className="Card-Row">
-        <label>Name:</label>
-        <h4>{name}</h4>
+        <h1>{name}</h1>
       </div>
       <div className="Card-Row">
-        <label>Founded:</label>
-        <h4>{founded}</h4>
+        <h3><span>Founded</span> {founded ? founded : 'N/A'}</h3>
       </div>
       <div className="Card-Row">
-        <label>Seats:</label>
-        <h4>{seats}</h4>
+        <h4><span>Seats:</span> {seats}</h4>
       </div>
       <div className="Card-Row">
-        <label>Titles:</label>
-        <h4>{titles}</h4>
+        <h4><span>Titles:</span> {titles}</h4>
       </div>
       <div className="Card-Row">
-        <label>Coat Of Arms:</label>
-        <h4>{coatOfArms}</h4>
+        <h4><span>Coat Of Arms:</span> {coatOfArms}</h4>
       </div>
       <div className="Card-Row">
-        <label>Ancestral Weapons:</label>
-        <h4>{ancestralWeapons}</h4>
+        <h4><span>Ancestral Weapons:</span> {ancestralWeapons}</h4>
       </div>
       <div className="Card-Row">
-        <label>Word:</label>
-        <h4>{words}</h4>
+        {
+          words &&
+          <h4><span>Words:</span> {words}</h4>
+        }
       </div>
     </div>
   )
